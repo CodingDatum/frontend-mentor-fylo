b0db318312f79244a76e27773601bdfe99b8e35e
Matthew Cain <codingdatum@gmail.com>
2021-09-13 15:09:15 -0400

    Finished the mobile first features section with reveal on scroll functionality

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,12 +1,12 @@
 import "../styles/styles.css";
 // import "lazysizes";
 // import MobileMenu from './modules/MobileMenu.js';
-// import RevealOnScroll from "./modules/RevealOnScroll";
+import RevealOnScroll from "./modules/RevealOnScroll";
 // import StickyHeader from "./modules/StickyHeader";
 
 // let stickyHeader = new StickyHeader();
 // let mobileMenu = new MobileMenu();
-// new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
+new RevealOnScroll(document.querySelectorAll(".features-section__feature-card"), 60);
 // new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
 // let modal;
 
